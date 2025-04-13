a09c7626386bdc68241671deb9853f0d4444135e
Debangan Dey <36856262+Ddey07@users.noreply.github.com>
2025-04-13 16:32:04 -0400

    Code running for B=49, n=10, and added printing of an adhoc loss function

--- src/trainer.py
+++ src/trainer.py
@@ -133,13 +133,13 @@ def trainer(n_iter,
     for iter in tqdm(range(n_iter)):
         # compute sigmasq_lambda_beta
         X_V_X_star_X = torch.einsum('bi,ij,bj->b', X, V_X_star, X).sum()
-        print("Norm of X_V_X_star_X:", torch.norm(X_V_X_star_X))
+        #print("Norm of X_V_X_star_X:", torch.norm(X_V_X_star_X))
         sigmasq_lambda_beta = 1.0 / (((lambda_a2 * X_V_X_star_X) / lambda_b2) + (1.0 / sigmasq_beta))
         
         # compute mu_lambda_beta
         residual = Y - (M_S_star @ mu_W.T).T
         X_M_X_star_residual = torch.einsum('bi,ij,bj->b', X, M_X_star.T, residual).sum()
-        print("Norm of X_M_X_star_residual:", torch.norm(X_M_X_star_residual))
+        #print("Norm of X_M_X_star_residual:", torch.norm(X_M_X_star_residual))
         mu_lambda_beta = sigmasq_lambda_beta * (lambda_a2 / lambda_b2) * X_M_X_star_residual
         
         # lambda_b1
@@ -161,7 +161,7 @@ def trainer(n_iter,
         Sigma_W = torch.linalg.pinv(term1 + term2, rtol = 1e-3)
         
         # compute mu_W        
-        print("Norm of residual2:", torch.norm((M_S_star.T @ Y.T - mu_lambda_beta * M_S_star.T @ M_X_star @ X.T).T.flatten()))
+        #print("Norm of residual2:", torch.norm((M_S_star.T @ Y.T - mu_lambda_beta * M_S_star.T @ M_X_star @ X.T).T.flatten()))
 
         mu_W = (lambda_a2 / lambda_b2) * (Sigma_W @ (M_S_star.T @ Y.T - mu_lambda_beta * M_S_star.T @ M_X_star @ X.T).T.flatten()).reshape(n_blocks, n_locations)
 
@@ -222,7 +222,7 @@ def trainer(n_iter,
             #         break
             #     prev_loss = loss.item()
         
-        print("Pix_loss", loss.item())
+        #print("Pix_loss", loss.item())
 
         # Extract the updated parameters
         M_X_star = model_piX.current_M_X_star.clone().data 
@@ -251,27 +251,36 @@ def trainer(n_iter,
             #         break
             #     prev_loss = loss.item()
             
-        print("PiS_loss", loss.item())
+        #print("PiS_loss", loss.item())
 
 
         # Extract the updated parameters
         M_S_star = model_piS.current_M_S_star.clone().data
         V_S_star = model_piS.current_V_S_star.clone().data
-        print(f"Iter {iter+1}/{n_iter} | mu_lambda_beta: {mu_lambda_beta:.4f} | sigmasq_lambda_beta: {sigmasq_lambda_beta:.4f} | lambda_a1: {lambda_a1:.4f} | lambda_b1: {lambda_b1:.4f} | lambda_a2: {lambda_a2:.4f} | lambda_b2: {lambda_b2:.4f}")
+        print(f"Iter {iter+1}/{n_iter} | mu_lambda_beta: {mu_lambda_beta:.4f} | \n sigmasq_lambda_beta: {sigmasq_lambda_beta:.4f} | \n lambda_a1: {lambda_a1:.4f} | lambda_b1: {lambda_b1:.4f} | lambda_a2: {lambda_a2:.4f} | lambda_b2: {lambda_b2:.4f}")
         norm_M_X_star = torch.norm(M_X_star)
         norm_V_X_star = torch.norm(V_X_star)
         norm_M_S_star = torch.norm(M_S_star)
         norm_V_S_star = torch.norm(V_S_star)
         norm_mean_Rphi_inv = torch.norm(mean_Rphi_inv)
-
-        print(f"‣ ||M_X_star||: {norm_M_X_star:.4f}, ||V_X_star||: {norm_V_X_star:.4f}")
-        print(f"‣ ||M_S_star||: {norm_M_S_star:.4f}, ||V_S_star||: {norm_V_S_star:.4f}")
-        print(f"‣ ||E[R(ϕ)]⁻¹||: {norm_mean_Rphi_inv:.4f}")
-        print(f" ‣ ||Sigma_W||: {torch.norm(Sigma_W):.4f}")
-        print(f" ‣ cond(Sigma_W): {torch.linalg.cond(Sigma_W):.4e}")
-        print(f" ‣ cond(Rphi_inv): {torch.linalg.cond(mean_Rphi_inv):.4e}")
-        print(f"‣ ||mu_W||: {torch.norm(mu_W):.4f}")     
+        print(f"‣ ||M_X_star||: {norm_M_X_star:.4f}, ||V_X_star||: {norm_V_X_star:.4f} | "
+              f"‣ ||M_S_star||: {norm_M_S_star:.4f}, ||V_S_star||: {norm_V_S_star:.4f} | "
+              f"‣ ||E[R(ϕ)]⁻¹||: {norm_mean_Rphi_inv:.4f} | "
+              f"‣ cond(Rphi_inv): {torch.linalg.cond(mean_Rphi_inv):.4e} | "
+              f"‣ ||Sigma_W||: {torch.norm(Sigma_W):.4f} | "
+              f"‣ cond(Sigma_W): {torch.linalg.cond(Sigma_W):.4e} | "
+              f"‣ ||mu_W||: {torch.norm(mu_W):.4f}")
         
+        total_loss = torch.trace(Y.T @ Y)
+        total_loss += (mu_lambda_beta ** 2 + sigmasq_lambda_beta) * X_V_X_star_X
+        total_loss += torch.einsum('bi,ij,bj->b', mu_W, V_S_star, mu_W).sum()
+        total_loss += torch.trace(torch.block_diag(*[V_S_star] * n_blocks) @ Sigma_W)
+        total_loss -= 2 * mu_lambda_beta * X_M_X_star_residual
+        total_loss -= 2 * torch.einsum('bi,ij,bj->b', Y, M_S_star, mu_W).sum()
+        print(f"Total Loss: {torch.sqrt(total_loss/(n_locations * n_blocks)):.4f}")
+
+
+
         # print(f"Iteration {iter+1}/{n_iter} completed.")
         # print("mu_lambda_beta:", mu_lambda_beta)
         # print("sigmasq_lambda_beta:", sigmasq_lambda_beta)
