fa8c0ba1f47a5ffc5683d9a375c17ab51589ad5c
Debangan Dey <36856262+Ddey07@users.noreply.github.com>
2026-03-06 17:50:14 -0600

    n=20 simulation needed bigger memory

--- src/analysis_varyB_annealed_3.sh
+++ src/analysis_varyB_annealed_3.sh
@@ -3,10 +3,10 @@
 #SBATCH --partition=long
 #SBATCH --output=out_annealed/analysis_%A_%a.out
 #SBATCH --error=log_annealed/analysis_%A_%a.err
-#SBATCH --array=0-799     # <= MaxArraySize (1001 tasks). %200 is optional.
+#SBATCH --array=0-199     # <= MaxArraySize (1001 tasks). %200 is optional.
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --mem=20G
 
 set -euo pipefail
 
@@ -17,7 +17,7 @@ which python
 mkdir -p out_annealed log_annealed
 
 # ---- Static params ----
-B_vals=(49 81 100 121)
+B_vals=(100 121)
 n_i_vals=(20)
 phi=2.0
 total_seed=100
